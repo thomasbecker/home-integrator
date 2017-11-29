9bc13befdb06dd93406182fc9578b391fbde0aa0
Thomas Becker <thomas.becker00@gmail.com>
2017-11-29 18:19:41 +0100

    increase timeout for myHomeControl

--- app/interfaces/MyHomeControlCollector.scala
+++ app/interfaces/MyHomeControlCollector.scala
@@ -27,9 +27,9 @@ class MyHomeControlCollector {
     }
 
     // TODO: replace with some non blocking way
-    val sleepingRoomCo2 = Await.result(sleepingRoomCo2Future, 10 seconds)
-    val heatpumpPowerConsumption = Await.result(heatpumpPowerConsumptionFuture, 10 seconds)
-    val livingRoomTemp = Await.result(livingRoomTempFuture, 10 seconds)
+    val sleepingRoomCo2 = Await.result(sleepingRoomCo2Future, 30 seconds)
+    val heatpumpPowerConsumption = Await.result(heatpumpPowerConsumptionFuture, 30 seconds)
+    val livingRoomTemp = Await.result(livingRoomTempFuture, 30 seconds)
     val end = System.currentTimeMillis()
     val myHomeControlData = MyHomeControlData(heatpumpPowerConsumption, livingRoomTemp, sleepingRoomCo2)
     println(s"myHomeControl Result in ${end - start} millis: $myHomeControlData")
